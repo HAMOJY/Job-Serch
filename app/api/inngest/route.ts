3fa3603b9fe7d030f2ae11bbfa0761abcfe053a2
Ahmed R. Abdullah <ahmed@aihirearab.com>
2026-04-13 07:41:41 +0300

    feat: add auth welcome email Inngest job

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,9 @@
 import { serve } from 'inngest/next'
 import { inngest } from '@/lib/inngest'
 import { waitlistWelcome } from '@/inngest/waitlist-welcome'
+import { authWelcome } from '@/inngest/auth-welcome'
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [waitlistWelcome],
+  functions: [waitlistWelcome, authWelcome],
 })
